cbe1f1c1e3159754df6185155c2e9964046aee19
Margaret Schmit <mschmit@umail.ucsb.edu>
2016-09-30 18:31:24 -0700

    finished project

--- src/Rational.java
+++ src/Rational.java
@@ -54,7 +54,88 @@ public class Rational {
 	return new Rational(a.num * b.num,
 			    a.denom * b.denom);
     }
-
+c static int lcm (int a, int b){
+        int retval = 0;
+        int g = gcd(a, b);
+        
+        retval = Math.abs(a*b);
+        retval = retval/g;
+        
+        return retval;
+        
+    }
+    
+    public Rational plus(Rational r){
+        int lcm = lcm(r.getDenominator(), denom);
+        int mthis = lcm/denom;
+        int mr = lcm/r.getDenominator();
+        
+        int numThis = mthis*num;
+        int numr = mr*r.getNumerator();
+        
+        int retnum = numThis+numr;
+        
+        Rational retval = new Rational(retnum, lcm);
+        
+        
+        return retval;
+    }
+    
+    public static Rational sum(Rational a, Rational b){
+        int lcm = lcm(a.getDenominator(), b.getDenominator());
+        
+        int ma = lcm/a.getDenominator();
+        int mb = lcm/b.getDenominator();
+        
+        int numA = ma * a.getNumerator();
+        int numB = mb * b.getNumerator();
+        
+        Rational retval = new Rational((numA+numB), lcm);
+        
+        return retval;
+    }
+    
+    public Rational minus(Rational r){
+        Rational neg = new Rational((-1*r.getNumerator()), r.getDenominator());
+        
+        Rational retval = sum(this, neg);
+        
+        return retval;
+    }
+    
+    public static Rational difference(Rational a, Rational b){
+        Rational neg = new Rational((-1*b.getNumerator()), b.getDenominator());
+        
+        Rational retval = sum(a, neg);
+        
+        return retval;
+    }
+    
+    public Rational reciprocalOf(){
+        if(num == 0){
+            throw new ArithmeticException("denominator may not be zero");
+        }else{
+            Rational retval = new Rational(denom, num);
+            return retval;
+        }
+    }
+    
+    public Rational dividedBy(Rational r){
+        int retnum = num*r.getDenominator();
+        int retdenom = denom*r.getNumerator();
+        
+        Rational retval = new Rational(retnum, retdenom);
+        return retval;
+    }
+    
+    public static Rational quotient (Rational a, Rational b){
+        int retNom = a.getNumerator()*b.getDenominator();
+        int retDenom = a.getDenominator()*b.getNumerator();
+        
+        Rational retval = new Rational(retNom, retDenom);
+        
+        return retval;
+    }
     
     /** 
 	For testing getters.  
